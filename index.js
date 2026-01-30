1a339439689a119f036402e7f6df4712c080e659
xbella21 <xbella6873@gmail.com>
2026-01-30 16:32:25 -0500

    before migration to type

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello, World desde devtree!')
+import express from 'express';
+
+const app = express();
+
+//routing
+app.get('/',(req , res)=>{
+  res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server is running on: ', port);
+});
