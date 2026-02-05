167816b881265bb36a9ca26a32cec42adf02c185
xbella21 <xbella6873@gmail.com>
2026-02-04 19:11:25 -0500

    Creacion de modelo de usuario

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
